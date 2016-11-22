a6476e41713d3ee9829055c4e1318096e6470514
own-star <taras.honcharuk@gmail.com>
2016-11-22 22:17:44 +0200

    Add method PUT

--- src/news_handler.erl
+++ src/news_handler.erl
@@ -7,6 +7,7 @@
 -export([content_types_accepted/2]).
 -export([handle_json/2, handle_get/2]).
 
+-include_lib("stdlib/include/qlc.hrl").
 -include("news.hrl").
 
 %-record(news,{title = undefined, content = undefined, id}).
@@ -25,7 +26,7 @@ init(_Transport, _Req, _Opts) ->
 		    {upgrade, protocol, cowboy_rest}.
 
 allowed_methods(Req, State) ->
-	{[<<"GET">>, <<"POST">>], Req, State}.
+	{[<<"GET">>, <<"POST">>, <<"PUT">>], Req, State}.
 
 
 allow_missing_post(Req, Opts) ->
@@ -51,9 +52,11 @@ handle_json(Req, State) ->
 		true ->
 			Val = jsx:decode(Body),
 			io:format("Get body: ~p~n~p~n", [Body,Val]),
-			Insert = insert(Val, #news{}),
-			{ok, Req2} = cowboy_req:reply(200, [], Insert, Req1),
-			{true, Req2, State};
+			{Method, Req2} = cowboy_req:method(Req1),
+			{NewsId, Req3} = cowboy_req:binding(news_id, Req2),
+			Insert = handle_data(Val, Method, binary_to_integer(NewsId)),
+			{ok, Req4} = cowboy_req:reply(200, [], Insert, Req3),
+			{true, Req4, State};
 		false ->
 			{ok, Req2} =
 			 cowboy_req:reply(400, [], 
@@ -62,10 +65,44 @@ handle_json(Req, State) ->
 	end.
 
 handle_get(Req, State) ->
-	{News, Req1} = cowboy_req:binding(news_id, Req),
-	io:format("News: ~p~n", [News]),
-	{ok, Req2} =cowboy_req:reply(404, [], <<"{\"", News/binary, " not Found\"}">>,  Req1),
+	{NewsId, Req1} = cowboy_req:binding(news_id, Req),
+	io:format("News: ~p~n", [NewsId]),
+	List = case NewsId of
+		undefined ->
+			do(qlc:q([{X#news.id, X#news.udate, X#news.title} || 
+					X <- mnesia:table(news)]));
+		_ -> do(qlc:q([X || 
+					X <- mnesia:table(news), X#news.id =:= binary_to_integer(NewsId)]))
+	end,
+	Json = get_json(List, []),
+	io:format("Mnesia returned: ~p~n~p~n", [List, Json]),
+	{ok, Req2} =cowboy_req:reply(200, [], Json,  Req1),
 	{true, Req2, State}.
+
+handle_data(Val, <<"PUT">>, NewsId) ->
+	update(Val, NewsId);
+handle_data(Val, <<"POST">>, _NewsId) ->
+	insert(Val, #news{}).
+
+update(Val, NewsId) when is_integer(NewsId) ->
+		[News] = do(qlc:q([X || X <- mnesia:table(news),
+								X#news.id =:= NewsId])),
+	update(Val, News);
+update([{<<"title">>, Title} | Rest], #news{} = News0) ->
+	News = News0#news{title = Title},
+	update(Rest, News);
+update([{<<"content">>, Content} | Rest], #news{} = News0) ->
+	News = News0#news{content = Content},
+	update(Rest, News);
+update([], #news{} = News0) ->
+	Date = get_time(calendar:universal_time()),
+	News = News0#news{udate = Date},
+	F = fun() ->
+		mnesia:write(News)
+	end,
+	Ret = mnesia:transaction(F),
+	io:format("Mnesia returned: ~p~n", [Ret]),
+	<<"{\"Updated\"}">>.
 
 insert([], #news{content = undefined}) ->
 	io:format("missed content\n"),
@@ -115,3 +152,17 @@ get_time({{Year, Month, Day}, {Hour, Min, Sec}}) ->
 	BinSec = list_to_binary(integer_to_list(Sec)),
 	<<BinYear/binary, "/", BinMonth/binary, "/", BinDay/binary, " ",
 	BinHour/binary, ":", BinMin/binary, ":", BinSec/binary, " UTC">>.
+
+get_json([], Acc) ->
+	jsx:encode(Acc);
+get_json([{Id, UpdateDate, Title} | Rest], Acc) ->
+	BinId = list_to_binary(integer_to_list(Id)),
+	get_json(Rest, [[{<<"id">>, BinId}, {<<"update_time">>, UpdateDate}, {<<"title">>, Title}] | Acc]);
+get_json([{_, Id, Title, Content, CreateDate, UpdateDate}], _Acc) ->
+	BinId = list_to_binary(integer_to_list(Id)),
+	jsx:encode([{<<"id">>, BinId}, {<<"create_time">>, CreateDate}, {<<"update_time">>, UpdateDate}, {<<"title">>, Title}, {<<"content">>, Content}]).
+
+do(Q) ->
+	F = fun() -> qlc:e(Q) end,
+	{atomic, Val} = mnesia:transaction(F),
+	Val.
