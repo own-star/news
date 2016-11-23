ab3675a30eb0025d15fd1c19b1df27e6aa747311
own-star <taras.honcharuk@gmail.com>
2016-11-23 10:06:28 +0200

    Some cosmetics

--- src/news_handler.erl
+++ src/news_handler.erl
@@ -1,30 +1,25 @@
 -module(news_handler).
 
 -export([init/3]).
+
+%% REST Callbacks
 -export([allowed_methods/2]).
 -export([allow_missing_post/2]).
 -export([content_types_provided/2]).
 -export([content_types_accepted/2]).
 -export([delete_resource/2, delete_completed/2]).
+
+%% Handlers
 -export([handle_json/2, handle_get/2]).
 
 -include_lib("stdlib/include/qlc.hrl").
 -include("news.hrl").
 
-%-record(news,{title = undefined, content = undefined, id}).
-
-%init(Req, Opts) ->
-%	io:format("Init handler ~p~n", [self()]),
-%	case cowboy_req:method(Req) of
-%		{<<"GET">>, Req1} ->
-%			handle_get(Req1);
-%		{_, _} ->
-%			{cowboy_rest, Req, Opts}.
-%	end.
 
 init(_Transport, _Req, _Opts) ->
-	    io:format("Init: ~p~n", [self()]),
 		    {upgrade, protocol, cowboy_rest}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% REST CallBacks %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 allowed_methods(Req, State) ->
 	{[<<"GET">>, <<"POST">>, <<"PUT">>, <<"DELETE">>], Req, State}.
@@ -34,20 +29,17 @@ allow_missing_post(Req, Opts) ->
 	{true, Req, Opts}.
 
 content_types_accepted(Req, State) ->
-	io:format("Accepted: ~p~n", [Req]),
 	{[
 		{<<"application/x-www-form-urlencoded">>, handle_json},
 		{<<"application/json">>, handle_json}
 	], Req, State}.
 
 content_types_provided(Req, State) ->
-	io:format("Provided: ~p~n", [Req]),
 	{[
-		{<<"application/json">>, handle_get}
+		{<<"*/*">>, handle_get}
 	], Req, State}.
 
 delete_resource(Req, State) ->
-	io:format("Delete: ~p~n", [Req]),
 	{NewsId, Req1} = cowboy_req:binding(news_id, Req),
 	Result =case NewsId of
 		undefined -> <<"{\"news_not_found\"}">>;
@@ -59,13 +51,13 @@ delete_completed(Req, State) ->
 	{ok, Req1} = cowboy_req:reply(200, [], <<"{\"deleted\"}">>, Req),
 	{true, Req1, State}.
 
+%%%%%%%%%%%%%%%%%%%%%%%%% CallBack Handlers %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 handle_json(Req, State) ->
-	io:format("Json\n"),
 	{ok, Body, Req1} = cowboy_req:body(Req),
 	case jsx:is_json(Body) of
 		true ->
 			Val = jsx:decode(Body),
-			io:format("Get body: ~p~n~p~n", [Body,Val]),
 			{Method, Req2} = cowboy_req:method(Req1),
 			{NewsId, Req3} = cowboy_req:binding(news_id, Req2),
 			News = case NewsId of
@@ -84,7 +76,6 @@ handle_json(Req, State) ->
 
 handle_get(Req, State) ->
 	{NewsId, Req1} = cowboy_req:binding(news_id, Req),
-	io:format("News: ~p~n", [NewsId]),
 	List = case NewsId of
 		undefined ->
 			do(qlc:q([{X#news.id, X#news.udate, X#news.title} || 
@@ -93,9 +84,12 @@ handle_get(Req, State) ->
 					X <- mnesia:table(news), X#news.id =:= binary_to_integer(NewsId)]))
 	end,
 	Json = get_json(List, []),
-	io:format("Mnesia returned: ~p~n~p~n", [List, Json]),
 	{ok, Req2} =cowboy_req:reply(200, [], Json,  Req1),
 	{true, Req2, State}.
+
+
+
+%%%%%%%%%%%%%%%%%%%  Privat Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 handle_data(Val, <<"PUT">>, NewsId) ->
 	update(Val, NewsId);
@@ -107,7 +101,6 @@ delete(NewsId) ->
 		mnesia:delete({news, NewsId})
 	end,
 	{Res, _} = mnesia:transaction(F),
-	io:format("Mnesia has returned: ~p~n", [Res]),
 	case Res of
 		atomic -> true;
 		aborted -> false
@@ -129,44 +122,43 @@ update([], #news{} = News0) ->
 	F = fun() ->
 		mnesia:write(News)
 	end,
-	Ret = mnesia:transaction(F),
-	io:format("Mnesia returned: ~p~n", [Ret]),
-	<<"{\"Updated\"}">>.
+	{Res, _} = mnesia:transaction(F),
+	case Res of 
+		aborted -> <<"{\"error\":\"not_updated\"}">>;
+		atomic -> <<"{\"success\":\"updated\"}">>
+	end.
 
 insert([], #news{content = undefined}) ->
-	io:format("missed content\n"),
-	<<"{\"field_content_not_presents\"}">>;
+	<<"{\"error\":\"field_content_not_presents\"}">>;
 insert([], #news{title = undefined}) ->
-	io:format("missed title\n"),
-	<<"{\"field_title_not_present\"}">>;
+	<<"{\"error\":\"field_title_not_present\"}">>;
 insert([], Acc) ->
-	io:format("insert all row\n"),
 	Q = fun() ->
 		mnesia:all_keys(news)
 	end,
 	{atomic, Keys} = mnesia:transaction(Q),
 	Next = get_next_id(Keys),
 	Date = get_time(calendar:universal_time()),
-	io:format("Date: ~p~n", [Date]),
 	Acc1 = 	Acc#news{id = Next, cdate = Date, udate = Date},
 	F = fun() ->
 		mnesia:write(Acc1)
 	end,
-	Ret = mnesia:transaction(F),
-	io:format("Mnesia returned: ~p~n", [Ret]),
-	BinNext = list_to_binary(integer_to_list(Next)),
-	<<"{\"id\":\"", BinNext/binary, "\",\"create_time\":\"", Date/binary, "\"}">>;
+	{Res , _} = mnesia:transaction(F),
+	case Res of
+		atomic ->
+			BinNext = list_to_binary(integer_to_list(Next)),
+			<<"{\"id\":\"", BinNext/binary, "\",\"create_time\":\"", Date/binary, "\"}">>;
+		aborted ->
+			<<"{\"error\":\"not_added\"}">>
+	end;
 insert([{<<"content">>, Content}|T], Acc) ->
-	io:format("insert content\n"),
 	Acc1 = Acc#news{content = Content},
 	insert(T, Acc1);
 insert([{<<"title">>, Title}|T], Acc) ->
-	io:format("insert title\n"),
 	Acc1 = Acc#news{title = Title},
 	insert(T, Acc1);
-insert(Val, Acc) ->
-	io:format("Bad match: ~p~n~p~n", [Val,Acc]),
-	<<"{\"bad_match\"}">>.
+insert(_Val, _Acc) ->
+	<<"{\"error\":\"bad_match\"}">>.
 
 get_next_id([]) -> 1;
 get_next_id(Keys) ->
